8ce3ccece5ec7fa0dcf0b0667383258ac856c1f4
Vinicius-Scodes <vinicius.s.pires.2004@gmail.com>
2024-12-02 10:00:37 -0300

    Conceitos básicos de variáveis.

---
+++ Aula2.py
@@ -0,0 +1,13 @@
+print("Aoooooo Coisa Booaa!!!")
+#Notificações por texto devem sempre estar entre " ou '
+#Para que o comando print seja executado é necessario () ao lado e o texto no interior com ""
+#
+nome="Vinicius"
+idade=20
+altura=1.80
+maior_de_idade=True
+#Tipos de variavel em ordem 1=string ; 2=integer ; 3=float ; 4=bool
+#Variaveis BOOLEANS tem que começar com a letre em maiusculo
+#
+print("seja bem-vindo",nome,"sua idade é",idade,"você mede",altura,"você é maior de idade?",maior_de_idade)
+#para apresentar uma variavel é preciso que a mesma não esteja com parenteses 
